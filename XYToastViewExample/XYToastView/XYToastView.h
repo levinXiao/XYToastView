61c12a7c0db6efd682c673dea77930a256b6e067
levinXiao <772381545@qq.com>
2016-05-06 15:18:51 +0800

    Update XYToastView.h

--- XYToastViewExample/XYToastView/XYToastView.h
+++ XYToastViewExample/XYToastView/XYToastView.h
@@ -1,14 +1,27 @@
 //
-//  XYToastView.h
+//  BYToastView.h
+//  ByidsMIOAS
 //
-//  Created by xiaoyu on 15/3/6.
+//  Created by 肖宇 on 15/3/6.
 //  Copyright (c) 2015年  All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface XYToastView : NSObject
+/**
+ *  该类实现了ios版本类似安卓上的toast的提示功能
+ *  
+ *  已知问题:在键盘弹出的时候 view会被键盘挡住 而看不见
+ *  
+ *  该view默认是在keywindow中显示
+ */
+@interface BYToastView : NSObject
 
 +(void)showToastWithMessage:(NSString *)messageString;
+
+@end
+
+@interface UIApplication (KeyBoardNotification)
+
 
 @end
